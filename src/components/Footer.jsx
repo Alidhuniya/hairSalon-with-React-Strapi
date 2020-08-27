7f362b145360eeb644c4a69bd08c5e4732a012c3
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-08-27 14:19:24 -0700

    responsive footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from 'react';
+import Navbar from './Navbar';
+import "./../sass/3-layout/_footer.scss";
 
 
 export default class Footer extends Component {
@@ -6,7 +8,31 @@ export default class Footer extends Component {
         return (
 
             <Fragment>
-                Footer
+               <footer>
+                   <div className = "footer__nav">
+                       <Navbar />
+                   </div>
+
+                   <div className = "footer__btmContents">
+
+                    <div className = "footer__socialLinks">
+
+                    <i className="fab fa-facebook fa-2x footer__icon"></i>
+                    <i className="fab fa-twitter fa-2x footer__icon"></i>
+                    <i className="fab fa-instagram fa-2x footer__icon"></i>
+                    <i className="fab fa-youtube fa-2x footer__icon"></i>
+
+                    </div>
+
+                    <div className = "claims">
+
+                    <h4 className = "claims__h40">Design and Developed by Ali Dhuniya</h4>
+                    <h4 className = "claims__h41">&copy;All Right Reserved</h4>
+
+                    </div>
+                      
+                   </div>
+               </footer>
             </Fragment>
             
         );
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDom from 'react-dom';
 // import App from "./App";
 import "./sass/basic-styles-scss/abstract-and-base.scss";
-import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import {
   BrowserRouter as Router
 } from "react-router-dom";
  
-ReactDom.render(<Router><Navbar /></Router>, document.getElementById("root"))
+ReactDom.render(<Router><Footer /></Router>, document.getElementById("root"));
 
 // pwa
 // if ('serviceWorker' in navigator) {
