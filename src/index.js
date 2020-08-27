7a69c22e19b5b34ede1e34ef5d03653ebd2b8149
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-08-26 20:06:52 -0700

    sass setup added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from "./App";
-import "./index.scss";
+// import App from "./App";
+import "./sass/basic-styles-scss/abstract-and-base.scss";
+import Navbar from './components/Navbar';
 import {
   BrowserRouter as Router
 } from "react-router-dom";
  
-ReactDom.render(<Router><App /></Router>, document.getElementById("root"))
+ReactDom.render(<Router><Navbar /></Router>, document.getElementById("root"))
 
 // pwa
 if ('serviceWorker' in navigator) {
