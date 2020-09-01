03171fbdc718de8a3f79ea7a4cc06d5330abe54b
Ali Dhuniya <alihussaindhuniya0307@gmail.com>
2020-08-31 18:58:09 -0700

    our services page added

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,12 +1,29 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from 'react';
+import Servicesection from './../components/Servicesection';
+import Servicesectionone from './../components/Servicesectionone';
+import Servicepricing from './../components/Servicepricing';
+import "./../sass/4-pages/_service.scss";
 
 export default class Services extends Component {
     render() {
         return (
            
                 <Fragment>
+                    <div className = "servicepage" >
 
-                <h1>Services</h1>
+                    <section>
+                        <Servicesection />
+                    </section>
+                        
+                    <section>
+                    <Servicesectionone />
+                        </section>
+                        
+                        <section className = "pricingcomp">
+                        <Servicepricing  />
+                        </section>
+
+                    </div>
                 </Fragment>
           
         )
